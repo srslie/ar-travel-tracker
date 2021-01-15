468d14d0bac44642c8ffcf1deaae1d1ea848dd66
srslie <aliceruppert@gmail.com>
2021-01-15 02:34:20 -0700

    Add destination test and passing class

--- src/Destination.js
+++ src/Destination.js
@@ -1,7 +1,25 @@
 class Destination {
-  constructor() {
-
+  constructor(destination) {
+      this.id = destination["id"];
+      this.destination = destination["destination"];
+      this.estimatedLodgingCostPerDay = destination["estimatedLodgingCostPerDay"];
+      this.estimatedFlightCostPerPerson = destination["estimatedFlightCostPerPerson"];
+      this.image = destination["image"];
+      this.alt = destination["alt"]
   }
+
+  calculateFee(subtotal) {
+    return subtotal + (subtotal * .1) 
+  }
+
+  //for either lodging Or Flight
+  calculateCost(num, priceProperty) {
+    const pricePerNum = this[priceProperty]
+    const stayCost = pricePerNum * num
+    const totalWithFee = this.calculateFee(stayCost)
+    return totalWithFee
+  }
+
 }
 
 export default Destination;
--- test/Destination-test.js
+++ test/Destination-test.js
@@ -1,13 +1,13 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-import Destination from '..src/Destination'
+import Destination from '../src/Destination'
 import destinations from './destinations-test-data'
 
 describe('Destination', function() {
   let lima, stockholm, sydney, cartagena, madrid
 
-  beforeEach(() => {
+  beforeEach(function() {
     lima = new Destination(destinations["destinations"][0])
     stockholm = new Destination(destinations["destinations"][1]) 
     sydney = new Destination(destinations["destinations"][2])
@@ -15,20 +15,36 @@ describe('Destination', function() {
     madrid = new Destination(destinations["destinations"][4]) 
   });
 
-  it('should be a function', () => {
+  it('should be a function', function() {
     expect(Destination).to.be.a('function');
   });
 
-  it('should be an instance of Destination', () => {
+  it('should be an instance of Destination', function() {
     expect(lima).to.be.an.instanceOf(Destination)
   });
 
-  it(' ', () => {
-    expect( ).to.
+  it('should have properties', function() {
+    expect(lima.id).to.equal(1)
+    expect(stockholm.destination).to.equal("Stockholm, Sweden")
+    expect(sydney.estimatedLodgingCostPerDay).to.equal(130)
+    expect(sydney.estimatedFlightCostPerPerson).to.equal(950)
+    expect(madrid.image).to.equal("https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
+    expect(madrid.alt).to.equal("city with clear skys and a road in the day time")
   });
 
-  it(' ', () => {
-    expect( ).to.
+  it('should calculate 10% travel agent fee', function() {
+    expect(lima.calculateFee(100)).to.equal(110)
+    expect(cartagena.calculateFee(46.50)).to.equal(51.15)
+  });
+
+  it('should calculate price with fee for lodging', function() {
+    expect(sydney.calculateCost(5, "estimatedLodgingCostPerDay")).to.equal(715)
+    expect(stockholm.calculateCost(2, "estimatedLodgingCostPerDay")).to.equal(220)
+  });
+
+  it('should calculate price with fee for flights', function() {
+    expect(stockholm.calculateCost(1, "estimatedFlightCostPerPerson")).to.equal(858)
+    expect(madrid.calculateCost(10, "estimatedFlightCostPerPerson")).to.equal(7150)
   });
 
 });
